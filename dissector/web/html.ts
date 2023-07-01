56b0cb10852dc32594fe959d014a8995e6b5b87e
Han Seung Min - 한승민 <hanseungmin.ar@gmail.com>
2023-07-01 22:40:49 +0900

    add doc

--- dissector/web/html.ts
+++ dissector/web/html.ts
@@ -1,6 +1,22 @@
 import assert from "assert";
 import { load } from "cheerio";
 
+/**
+ * This function returns data from the given HTML file source.
+ *
+ * There are 3 things you'll need from the HTML file:
+ * 1. The GLOBAL_ENV.
+ * 2. The base stylesheet.
+ * 3. The surface level JS chunks.
+ *
+ * The GLOBAL_ENV is useful if you want to do something like
+ * <https://gitlab.com/bignutty/displunger> or <https://github.com/nurmarvin/discord-proxy>.
+ *
+ * The base stylesheet isn't that useful unless you have some specific use for it.
+ *
+ * The surface level chunks are the important ones as they'll allow you to perform
+ * other things like getting assets or extracting strings.
+ */
 export function processHtml(html: string) {
     const $ = load(html);
 
